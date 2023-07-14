a2e262f414caf416bfdd840b6dbb0ded1cf3b618
victor <victorharris200@gmail.com>
2023-07-13 17:26:26 -0700

    Cis_good

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
+ *main - Entry point
  *Return: always 0 (sucess)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
