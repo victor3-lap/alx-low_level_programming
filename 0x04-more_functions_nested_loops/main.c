b472b2c2dd17ad1fe9daad467812649b9280c595
Victor <victor@linx.myguest.virtualbox.org>
2023-07-21 18:47:51 +0100

    last commit

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,18 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
+    print_square(2);
+    print_square(10);
+    print_square(0);
     return (0);
 }
