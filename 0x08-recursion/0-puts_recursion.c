dd19f792e0beb9298317f3bebcce242416355fa3
Victor <victor@Linux.myguest.virtualbox.org>
2023-08-02 19:18:27 +0100

    last one

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,18 @@
 #include "main.h"
+
 /**
- * _puts_recursion - prints string,
- * followed by a new line
- * @s : char to be used in the function
- * Return - return 0
+ * _puts_recursion- prints a string, followed by a new line.
+ * @s: string to work on.
+ * Return: nothing.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == "\0")
-
-		_putchar('\n');
-
-	else
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
 
-
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - print string
- * @s: string to be print
- * Return - Nothing
+ * _print_rev_recursion- prints a string in reverse.
+ * @s: string to print in reverse.
+ * Return: nothingg.
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_purchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else
-	_putchar('\n');
 }
